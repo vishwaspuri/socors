a56a4e8a86780477836020b1b154012630312bf7
vishwaspuri <vishwaspuri09@gmail.com>
2020-05-26 19:03:21 +0530

    Sign-up integrated

--- user/static/js/signup.js
+++ user/static/js/signup.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     var $submitBtn = $("#submit");
     var $passwordBox = $("#password");
     var $confirmBox = $("#confirm-password");
@@ -23,6 +24,15 @@ $(document).ready(function(){
         }  
     }
 
+    focusTwo = function getFocus() {           
+        document.getElementById("two").focus();
+    }
+    focusThree= function getFocus() {           
+        document.getElementById("three").focus();
+    }
+    focusFour = function getFocus() {  
+            document.getElementById("four").focus();
+    }
 
     $("#confirm-password, #password")
          .on("keydown", function(e){
@@ -38,4 +48,76 @@ $(document).ready(function(){
         .on("focus", function(){
             resetPasswordError();
         })
-})
+
+        const form= {
+            phone: document.getElementById("phone"),
+            full_name: document.getElementById("full_name"),
+            email: document.getElementById("email"),
+            password: document.getElementById("password"),
+            submit: document.getElementById("submit")
+        }
+                
+        form.submit.addEventListener("click", function(){
+            const request = new XMLHttpRequest();
+        
+            request.onload= function(){
+                var responseJSON= JSON.parse(request.responseText);
+                if(responseJSON.status){
+                        document.getElementsByClassName("center")[1].innerHTML="<p>Swipe to Continue</p>";
+                        document.getElementsByClassName("otp")[0].innerHTML='<img src="../images/holding_mobile_phone.jpg" alt="hand_holding_mobile"><h1>Enter OTP</h1><div id="otp-form">'+
+                            '<input id="one" type="text" onkeypress="focusTwo()" required>'+
+                            '<input id="two" type="text" onkeypress="focusThree()" required>'+
+                            '<input id="three" type="text" onkeypress="focusFour()" required>'+
+                            '<input id="four" type="text" required>'+
+                    
+                            '<input type="submit" value="submit" id="otp-submit">'+
+                        '</div>';
+
+                        const OTPform={
+                        phone: form.phone.value,
+                        otp: document.getElementById("otp-form"),
+                        submit: document.getElementById("otp-submit")
+                    }
+
+                    
+                    OTPform.submit.addEventListener("click", function(){
+                        const OTPrequest= new XMLHttpRequest();
+                    
+                        OTPrequest.onload=function(){
+                            const OTPresponse=JSON.parse(this.responseText);
+                            if(OTPresponse.status==true){
+                                document.getElementById("next").classList.add("swiper-button-next");
+                                document.getElementById("next").classList.remove("swiper-button-disabled");
+                            }
+                        }
+                    
+                        const OTPRequestData={
+                            "phone": form.phone.value,
+                            "otp": OTPform.otp.children[0].value+OTPform.otp.children[1].value+OTPform.otp.children[2].value+OTPform.otp.children[3].value
+                        }
+                    
+                        const OTPstring=JSON.stringify(OTPRequestData);
+                        console.log(OTPstring)
+                        OTPrequest.open('post',"https://vishwastest.pythonanywhere.com/user/api/otp/");
+                        OTPrequest.setRequestHeader('Content-type', 'application/json');
+                        OTPrequest.send(OTPstring);
+                    })
+                }
+                else{
+                    console.log(this.responseText)
+                }
+            }
+        
+            const requestData={
+                "phone": form.phone.value,
+                "full_name": form.full_name.value,
+                "email": form.email.value,
+                "password": form.password.value
+            }
+        
+            const reqJSON=JSON.stringify(requestData)
+            request.open('post', 'https://vishwastest.pythonanywhere.com/user/api/validate-phone/');
+            request.setRequestHeader('Content-type', 'application/json');
+            request.send(reqJSON);
+        })
+});
