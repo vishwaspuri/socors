a80c6ce4fbece9d51b1e683c7769eb7a2c9084e0
kshitiz2099 <65618212+kshitiz2099@users.noreply.github.com>
2020-05-30 20:06:36 +0530

    Update menu.js

--- main/static/js/menu.js
+++ main/static/js/menu.js
@@ -95,24 +95,5 @@ $(document).ready(function(){
                     }
                 }
             xhr.send();
-        }
-    var editNameIcon=$("#edit_name");
-    var name=$("#nav_name");
-    var currentName=name.text();
-    var divPicture=$('.picture')
-    divPicture.on("click", $('sup'),function(event){
-                
-                if((!name.html().includes('<input')) &&(event.target.localName== "sup" || event.target.localName == "img")){
-                    name.html("<input id='nameEdit' type='text'><sup><img id='edit_name' src='../static/images/edit_icon.png' alt=></sup>");
-                    var nameInput=$('#nameEdit')[0];
-                    nameInput.value=currentName;
-                }
-
-                else if((name.html().includes('<input')) &&(event.target.localName== "sup" || event.target.localName == "img")){
-                    var nameInput=$('#nameEdit')[0];
-                    currentName=nameInput.value;
-                    name.html(currentName+'<sup><img id="edit_name" src="../static/images/edit_icon.png" alt=""></sup>');
-               
-                }
-    });  
+        }  
 });
