a2f012b4078e08d4aa8015668b755e411f18f4f8
vishwaspuri <vishwaspuri09@gmail.com>
2020-06-13 18:10:33 +0530

    postgres database added

--- socors_backend/settings.py
+++ socors_backend/settings.py
@@ -96,14 +96,26 @@ WSGI_APPLICATION = 'socors_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+
+
+if DEBUG:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'socorsuserapp',
+        'USER': 'socorsadmin',
+        'PASSWORD': 'socors@123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
