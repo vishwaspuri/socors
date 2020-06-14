01cf45b53fc439119f6a83df08ad677b1d2a0245
vishwaspuri <vishwaspuri09@gmail.com>
2020-06-14 12:49:33 +0530

    home.html changes

--- main/static/js/home.js
+++
@@ -1,6 +0,0 @@
-$(".swipeBtn").on("click",function(){
-    console.log("click click");
-    if($('.swiper-button-next').hasClass('swiper-button-disabled')){
-            $('.swipeBtn').html("<a href='http://socorsnearyou.xyz/user/signup/'><div class='swiper-button-next swiper-button-disabled'></div></a>");
-    }
-});
