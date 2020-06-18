8db5392fa09e9bbb04c9347e76d2db133f6437e0
vishwaspuri <vishwaspuri09@gmail.com>
2020-06-19 01:37:34 +0530

    mini-changes

--- socors_backend/settings.py
+++ socors_backend/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 
 
