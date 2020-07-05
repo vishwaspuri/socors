d0b505ab0e777f04681001083b78d50d56aeb558
vishwaspuri <vishwaspuri09@gmail.com>
2020-07-05 15:11:14 +0530

    Added Custom auth backend ++

--- user/CustomAuthenticationBackend.py
+++ user/CustomAuthenticationBackend.py
@@ -4,7 +4,7 @@ from django.contrib.auth.backends import ModelBackend
 MyUser = get_user_model()
 
 class PhNumberOrEmailBackend(ModelBackend):
-    def authenticate(self, request,username=None, password=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
         try:
             user = MyUser.objects.get(Q(email=username) | Q(ph_number=username))
             if user.check_password(password):
